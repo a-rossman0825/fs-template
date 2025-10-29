f7ce2c4772db32511102ee150c622cdd2ba70c15
a-rossman0825 <anthony.rossman0825@gmail.com>
2025-10-29 11:46:05 -0600

    stop tracking env.ts

--- client/src/env.ts
+++
@@ -1,6 +0,0 @@
-export const dev = window.location.origin.includes('localhost');
-export const baseURL = dev ? 'https://localhost:7045' : '';
-export const useSockets = false;
-export const domain = 'dev-gp7tg4z8hq16f7fn.us.auth0.com';
-export const clientId = 'wZT8GTLfsWqirIkH3B2SN2cuEBzK6rst';
-export const audience = 'https://testserver.com';
