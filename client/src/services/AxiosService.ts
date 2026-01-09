bc0e74b61e84eab3825332ec2926619728d6cc5d
a-rossman0825 <anthony.rossman0825@gmail.com>
2026-01-09 13:53:45 -0700

    Linter errors - FIXED

--- client/src/services/AxiosService.ts
+++ client/src/services/AxiosService.ts
@@ -1,23 +1,23 @@
-import Axios from 'axios';
-import { baseURL } from '@/env';
-import { logger } from '@/utils/Logger';
-import type { AxiosError } from 'axios';
+import Axios from "axios";
+import { baseURL } from "@/env";
+import { logger } from "@/utils/Logger";
+import type { AxiosError } from "axios";
 
 export const api = Axios.create({
   baseURL,
-  timeout: 8000
+  timeout: 8000,
 });
 
-api.interceptors.request.use(config => config, handleAxiosError);
-api.interceptors.response.use(response => response, handleAxiosError);
+api.interceptors.request.use((config) => config, handleAxiosError);
+api.interceptors.response.use((response) => response, handleAxiosError);
 
-function handleAxiosError(error: AxiosError){
+function handleAxiosError(error: AxiosError) {
   if (error.response) {
-    logger.warn('[📡 AXIOS_ERROR_RESPONSE_DATA]', error.response.data);
-  } else if (error.request){
-    logger.warn('[📡 AXIOS_ERROR_NO_RESPONSE]', error.request);
+    logger.warn("[📡 AXIOS_ERROR_RESPONSE_DATA]", error.response.data);
+  } else if (error.request) {
+    logger.warn("[📡 AXIOS_ERROR_NO_RESPONSE]", error.request);
   } else {
-    logger.warn('[📡 AXIOS_ERROR_INVALID_REQUEST]', error.message);
+    logger.warn("[📡 AXIOS_ERROR_INVALID_REQUEST]", error.message);
   }
   return Promise.reject(error);
 }
